b98b9ed354999fe3d2286bdd27e83ffd43807f20
github username <github email address>
2019-05-20 17:33:45 -0400

    kata

--- helloworld.py
+++ helloworld.py
@@ -4,3 +4,32 @@ print("Hello World")
 
 if 5 > 2:
     print("good work")
+
+x = "me"
+y = 10
+
+print(x)
+print(y)
+
+
+# Codewars
+
+def xo(s):
+    array = s.split()
+    xs = 0
+    os = 0
+    x = "x"
+    o = "o"
+    for item in array:
+        if item == o:
+            os += 1
+        elif item == x:
+            xs += 1
+
+    if xs == os:
+        return True
+
+
+def xo(s):
+    s = s.lower()
+    return s.count('x') == s.count('o')
